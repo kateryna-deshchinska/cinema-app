05405484da1df36583254b85c56302981a883a42
Kate Deshchynska <katerinka9636@gmail.com>
2022-01-04 18:04:34 +0200

    remove empty line

--- src/main/java/cinema/util/HashUtil.java
+++ src/main/java/cinema/util/HashUtil.java
@@ -11,15 +11,15 @@ public class HashUtil {
     }
 
     public static String hashPassword(String password, byte[] salt) {
-        StringBuilder hashedPwd = new StringBuilder();
+        StringBuilder hashedPassword = new StringBuilder();
         try {
             MessageDigest messageDigest = MessageDigest.getInstance(HASH_ALGORITHM);
             messageDigest.update(salt);
             byte[] digest = messageDigest.digest(password.getBytes());
             for (byte b : digest) {
-                hashedPwd.append(String.format("%02x", b));
+                hashedPassword.append(String.format("%02x", b));
             }
-            return hashedPwd.toString();
+            return hashedPassword.toString();
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException("Can`t hash password!", e);
         }
